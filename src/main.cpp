15d9d4dcdbcd2eee00762aaa1c820529c74aed0b
André Biseth <andre@biseth.net>
2024-08-09 23:17:14 +0000

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,9 @@
 #include <GxEPD2_3C.h>
 #include <GxEPD2_4C.h>
 #include <GxEPD2_7C.h>
-//#include <Fonts/FreeSansBold24pt7b.h>
-#include <../fonts/Roboto_Black48pt7b.h>
+// #include <Fonts/FreeSansBold24pt7b.h>
+#include <../fonts/RobotoCondensed_Bold48pt7b.h>
+#include <../fonts/Roboto_Regular10pt7b.h>
 
 // select the display class and display driver class in the following file (new style):
 #include "GxEPD2_display_selection_new_style.h"
@@ -45,32 +46,51 @@
 
 void write_temp(float temp)
 {
-    char screen_text[10]; // Make sure this is large enough to hold the formatted string
-    sprintf(screen_text, "%.1f° C", temp); // Convert float to string with 1 decimal places
+    const char *info_text = "Siste: 2024-08-09 17:18:42";
+    char temp_text[10];                  // Make sure this is large enough to hold the formatted string
+    sprintf(temp_text, "%.1f C", temp); // Convert float to string with 1 decimal places
 
     // Replace the decimal point with a comma
-    char* comma_pos = strchr(screen_text, '.');
+    char *comma_pos = strchr(temp_text, '.');
     if (comma_pos)
     {
         *comma_pos = ','; // Replace '.' with ','
     }
 
     display.setRotation(1);
-    display.setFont(&Roboto_Black48pt7b);
     display.setTextColor(GxEPD_BLACK);
-    int16_t tbx, tby;
-    uint16_t tbw, tbh;
-    display.getTextBounds(screen_text, 0, 0, &tbx, &tby, &tbw, &tbh);
+
+    display.setFont(&RobotoCondensed_Bold48pt7b);
+    int16_t tb1_x, tb1_y;
+    uint16_t tb1_w, tb1_h;
+    display.getTextBounds(temp_text, 0, 0, &tb1_x, &tb1_y, &tb1_w, &tb1_h);
     // center the bounding box by transposition of the origin:
-    uint16_t x = ((display.width() - tbw) / 2) - tbx;
-    uint16_t y = ((display.height() - tbh) / 2) - tby;
+    uint16_t x1 = ((display.width() - tb1_w) / 2) - tb1_x;
+    uint16_t y1 = tb1_h - 15;
+
+    display.setFont(&Roboto_Regular10pt7b);
+    int16_t tb2_x, tb2_y;
+    uint16_t tb2_w, tb2_h;
+    display.getTextBounds(info_text, 0, 0, &tb2_x, &tb2_y, &tb2_w, &tb2_h);
+    // center the bounding box by transposition of the origin:
+    uint16_t x2 = ((display.width() - tb2_w) / 2) - tb2_x;
+    uint16_t y2 = display.height() - 5;
+
     display.setFullWindow();
     display.firstPage();
     do
     {
         display.fillScreen(GxEPD_WHITE);
-        display.setCursor(x, y);
-        display.print(screen_text);
+        // display.writeFastHLine(0, 0, display.width(), GxEPD_BLACK);
+        // display.writeFastHLine(0, display.height() - 1, display.width(), GxEPD_BLACK);
+        // display.writeFastVLine(0, 0, display.height(), GxEPD_BLACK);
+        // display.writeFastVLine(display.width() - 1, 0, display.height(), GxEPD_BLACK);
+        display.setCursor(x1, y1);
+        display.setFont(&RobotoCondensed_Bold48pt7b);
+        display.print(temp_text);
+        display.setFont(&Roboto_Regular10pt7b);
+        display.setCursor(x2, y2);
+        display.print(info_text);
     } while (display.nextPage());
 }
 
@@ -78,7 +98,7 @@ void setup()
 {
     // display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
     display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
-    write_temp(12.3f);
+    write_temp(20.4f);
     display.hibernate();
 }
 
